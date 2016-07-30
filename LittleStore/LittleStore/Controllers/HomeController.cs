9d52dd97277d9f0b842143098b999d8b45a582e0
Ignat <IgnatParser@gnail.com>
2016-07-30 17:46:36 +0300

    ver: 0.0.2,   Начальный дизайн

--- LittleStore/LittleStore/Controllers/HomeController.cs
+++ LittleStore/LittleStore/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LittleStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,9 +12,13 @@ namespace LittleStore.Controllers
         //
         // GET: /Home/
 
+        LittleStoreContext db = new LittleStoreContext();
         public ActionResult Index()
         {
-            return View();
+
+            var products = (from u in db.Products select u).ToList();
+
+            return View(products);
         }
 
     }
